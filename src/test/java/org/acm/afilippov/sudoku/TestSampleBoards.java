fc19a97bd055e97fdf40bdcae948073001b0a6c6
Alexey Filippov <afilippov@acm.org>
2012-02-11 22:55:53 +0000

    DRY: testing method extracted further

--- src/test/java/org/acm/afilippov/sudoku/TestSampleBoards.java
+++ src/test/java/org/acm/afilippov/sudoku/TestSampleBoards.java
@@ -10,56 +10,29 @@ import static org.acm.afilippov.sudoku.Variation.SUPER4X4;
 import static org.junit.Assert.assertTrue;
 
 public class TestSampleBoards {
-    @Test
-    public void sampleTaskBySearchSpring() throws IOException {
-        trySolving(classic("tasks.txt"));
-    }
+    public static final String[] CLASSIC_TASKS = {
+            "tasks.txt",
+            "hard.txt",
+            "hard2.txt",
+            "hard3.txt",
+            "evil.txt",
+            "evil2.txt",
+            "diabolical.txt"
+    };
 
     @Test
-    public void hardTask() throws IOException {
-        trySolving(classic("hard.txt"));
-    }
-
-    @Test
-    public void hardTask2() throws IOException {
-        trySolving(classic("hard2.txt"));
-    }
-
-    @Test
-    public void hardTask3() throws IOException {
-        trySolving(classic("hard3.txt"));
-    }
-
-    @Test
-    public void evilTask() throws IOException {
-        trySolving(classic("evil.txt"));
-    }
-
-    @Test
-    public void evilTask2() throws IOException {
-        trySolving(classic("evil2.txt"));
-    }
-
-    @Test
-    public void diabolicalTask() throws IOException {
-        trySolving(classic("diabolical.txt"));
+    public void classicTasks() throws IOException {
+        for (String task : CLASSIC_TASKS)
+            trySolving(task, readSudoku(task, CLASSIC));
     }
 
     @Test
     public void super4X4() throws IOException {
-        trySolving(super4x4("super4x4.txt"));
+        trySolving("super4x4.txt", readSudoku("super4x4.txt", SUPER4X4));
     }
 
-    private static void trySolving(Sudoku sudoku) {
+    private static void trySolving(String name, Sudoku sudoku) {
         sudoku.solve();
-        assertTrue(sudoku.isSolved());
-    }
-
-    private static Sudoku classic(final String filename) throws IOException {
-        return readSudoku(filename, CLASSIC);
-    }
-
-    private static Sudoku super4x4(final String filename) throws IOException {
-        return readSudoku(filename, SUPER4X4);
+        assertTrue("Cannot solve " + name, sudoku.isSolved());
     }
 }
